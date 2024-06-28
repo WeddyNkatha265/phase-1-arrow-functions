76a0a3812cef47e0aea1bf8bc82c817837c145e6
Weddy Nkatha <weddynkatha265@gmail.com>
2024-06-28 16:16:59 +0300

    updates arrow functions

---
+++ index.js
@@ -0,0 +1,12 @@
+const divide = (a, b) => {
+  return a / b;
+};
+divide(2000, 100);
+const square = (x) => {
+  return x ** 2;
+};
+square(2);
+const add = (a, b) => {
+  return a + b;
+};
+add(a, b);
